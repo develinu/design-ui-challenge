9e2325f6fa3dd09f24096a8287c444533eca5434
develinu <iwlee.dev@gmail.com>
2022-03-06 01:09:04 +0900

    feat: 디자인 리소스 추가
    - 디자인 리소스 추가
    - 디자인 메타데이터 추가

---
+++ src/data/imageMeta.js
@@ -0,0 +1,411 @@
+export const meta = [
+  {
+    "ChallengeNumber": 1,
+    "Title": "SignUp"
+  },
+  {
+    "ChallengeNumber": 2,
+    "Title": "CreditCardCheckout"
+  },
+  {
+    "ChallengeNumber": 3,
+    "Title": "LandingPage"
+  },
+  {
+    "ChallengeNumber": 4,
+    "Title": "Calculator"
+  },
+  {
+    "ChallengeNumber": 5,
+    "Title": "AppIcon"
+  },
+  {
+    "ChallengeNumber": 6,
+    "Title": "UserProfile"
+  },
+  {
+    "ChallengeNumber": 7,
+    "Title": "Setting"
+  },
+  {
+    "ChallengeNumber": 8,
+    "Title": "404Page"
+  },
+  {
+    "ChallengeNumber": 9,
+    "Title": "MusicPlayer"
+  },
+  {
+    "ChallengeNumber": 10,
+    "Title": "SocialShareIcon"
+  },  
+
+  {
+    "ChallengeNumber": 11,
+    "Title": "FlashMessage"
+  },
+  {
+    "ChallengeNumber": 12,
+    "Title": "E-CommerceShop"
+  },
+  {
+    "ChallengeNumber": 13,
+    "Title": "DirectMessage"
+  },
+  {
+    "ChallengeNumber": 14,
+    "Title": "CountdownTimer"
+  },
+  {
+    "ChallengeNumber": 15,
+    "Title": "On/OffSwitch"
+  },
+  {
+    "ChallengeNumber": 16,
+    "Title": "PopUp/Overlay"
+  },
+  {
+    "ChallengeNumber": 17,
+    "Title": "EmailReceipt"
+  },
+  {
+    "ChallengeNumber": 18,
+    "Title": "AnalyticsChart"
+  },
+  {
+    "ChallengeNumber": 19,
+    "Title": "LeaderBoard"
+  },
+  {
+    "ChallengeNumber": 20,
+    "Title": "LocationTracker"
+  },
+
+  {
+    "ChallengeNumber": 21,
+    "Title": "HomeMonitoringDashboard"
+  },
+  {
+    "ChallengeNumber": 22,
+    "Title": "Search"
+  },
+  {
+    "ChallengeNumber": 23,
+    "Title": "Onboarding"
+  },
+  {
+    "ChallengeNumber": 24,
+    "Title": "BoardingPass"
+  },
+  {
+    "ChallengeNumber": 25,
+    "Title": "TVApp"
+  },
+  {
+    "ChallengeNumber": 26,
+    "Title": "Subscribe"
+  },
+  {
+    "ChallengeNumber": 27,
+    "Title": "Dropdown"
+  },
+  {
+    "ChallengeNumber": 28,
+    "Title": "ContactUs"
+  },
+  {
+    "ChallengeNumber": 29,
+    "Title": "Map"
+  },
+  {
+    "ChallengeNumber": 30,
+    "Title": "Pricing"
+  },
+
+  {
+    "ChallengeNumber": 31,
+    "Title": "FileUpload"
+  },
+  {
+    "ChallengeNumber": 32,
+    "Title": "CrowdfundingCampaign"
+  },
+  {
+    "ChallengeNumber": 33,
+    "Title": "CustomizedProduct"
+  },
+  {
+    "ChallengeNumber": 34,
+    "Title": "CarInterface"
+  },
+  {
+    "ChallengeNumber": 35,
+    "Title": "BlogPost"
+  },
+  {
+    "ChallengeNumber": 36,
+    "Title": "SpecialOffer"
+  },
+  {
+    "ChallengeNumber": 37,
+    "Title": "Weather"
+  },
+  {
+    "ChallengeNumber": 38,
+    "Title": "Testimonials"
+  },
+  {
+    "ChallengeNumber": 39,
+    "Title": "Calendar"
+  },
+  {
+    "ChallengeNumber": 40,
+    "Title": "Recipe"
+  },
+
+  {
+    "ChallengeNumber": 41,
+    "Title": "WorkoutTracker"
+  },
+  {
+    "ChallengeNumber": 42,
+    "Title": "Food/DrinkMenu"
+  },
+  {
+    "ChallengeNumber": 43,
+    "Title": "TodoList"
+  },
+  {
+    "ChallengeNumber": 44,
+    "Title": "Favorites"
+  },
+  {
+    "ChallengeNumber": 45,
+    "Title": "InfoCard"
+  },
+  {
+    "ChallengeNumber": 46,
+    "Title": "Invoice"
+  },
+  {
+    "ChallengeNumber": 47,
+    "Title": "ActivityFeed"
+  },
+  {
+    "ChallengeNumber": 48,
+    "Title": "ComingSoon"
+  },
+  {
+    "ChallengeNumber": 49,
+    "Title": "Notification"
+  },
+  {
+    "ChallengeNumber": 50,
+    "Title": "JobListing"
+  },
+
+  {
+    "ChallengeNumber": 51,
+    "Title": "PressPage"
+  },
+  {
+    "ChallengeNumber": 52,
+    "Title": "Logo"
+  },
+  {
+    "ChallengeNumber": 53,
+    "Title": "Header"
+  },
+  {
+    "ChallengeNumber": 54,
+    "Title": "Confirmation"
+  },
+  {
+    "ChallengeNumber": 55,
+    "Title": "IconSet"
+  },
+  {
+    "ChallengeNumber": 56,
+    "Title": "Breadcrumbs"
+  },
+  {
+    "ChallengeNumber": 57,
+    "Title": "VideoPlayer"
+  },
+  {
+    "ChallengeNumber": 58,
+    "Title": "ShoppingCart"
+  },
+  {
+    "ChallengeNumber": 59,
+    "Title": "BackgroundPattern"
+  },
+  {
+    "ChallengeNumber": 60,
+    "Title": "ColorPicker"
+  },
+
+  {
+    "ChallengeNumber": 61,
+    "Title": "RedeemCoupon"
+  },
+  {
+    "ChallengeNumber": 62,
+    "Title": "WorkoutOfTheDay"
+  },
+  {
+    "ChallengeNumber": 63,
+    "Title": "BestOf"
+  },
+  {
+    "ChallengeNumber": 64,
+    "Title": "SelectUserType"
+  },
+  {
+    "ChallengeNumber": 65,
+    "Title": "NotesWidget"
+  },
+  {
+    "ChallengeNumber": 66,
+    "Title": "Statistics"
+  },
+  {
+    "ChallengeNumber": 67,
+    "Title": "HotelBooking"
+  },
+  {
+    "ChallengeNumber": 68,
+    "Title": "FlightSearch"
+  },
+  {
+    "ChallengeNumber": 69,
+    "Title": "Trending"
+  },
+  {
+    "ChallengeNumber": 70,
+    "Title": "EventListing"
+  },
+
+  {
+    "ChallengeNumber": 71,
+    "Title": "Schedule"
+  },
+  {
+    "ChallengeNumber": 72,
+    "Title": "ImageSlider"
+  },
+  {
+    "ChallengeNumber": 73,
+    "Title": "VirtualReality"
+  },
+  {
+    "ChallengeNumber": 74,
+    "Title": "DownloadApp"
+  },
+  {
+    "ChallengeNumber": 75,
+    "Title": "PreOrder"
+  },
+  {
+    "ChallengeNumber": 76,
+    "Title": "Loading"
+  },
+  {
+    "ChallengeNumber": 77,
+    "Title": "ThankYou"
+  },
+  {
+    "ChallengeNumber": 78,
+    "Title": "PendingInvitation"
+  },
+  {
+    "ChallengeNumber": 79,
+    "Title": "Itinerary"
+  },
+  {
+    "ChallengeNumber": 80,
+    "Title": "DatePicker"
+  },
+
+  {
+    "ChallengeNumber": 81,
+    "Title": "StatusUpdate"
+  },
+  {
+    "ChallengeNumber": 82,
+    "Title": "Form"
+  },
+  {
+    "ChallengeNumber": 83,
+    "Title": "Button"
+  },
+  {
+    "ChallengeNumber": 84,
+    "Title": "Badge"
+  },
+  {
+    "ChallengeNumber": 85,
+    "Title": "Pagination"
+  },
+  {
+    "ChallengeNumber": 86,
+    "Title": "ProgressBar"
+  },
+  {
+    "ChallengeNumber": 87,
+    "Title": "Tooltip"
+  },
+  {
+    "ChallengeNumber": 88,
+    "Title": "Avatar"
+  },
+  {
+    "ChallengeNumber": 89,
+    "Title": "TermsOfService"
+  },
+  {
+    "ChallengeNumber": 90,
+    "Title": "CreateNew"
+  },
+
+  {
+    "ChallengeNumber": 91,
+    "Title": "CuratedForYou"
+  },
+  {
+    "ChallengeNumber": 92,
+    "Title": "FAQ"
+  },
+  {
+    "ChallengeNumber": 93,
+    "Title": "Splash"
+  },
+  {
+    "ChallengeNumber": 94,
+    "Title": "News"
+  },
+  {
+    "ChallengeNumber": 95,
+    "Title": "ProductTour"
+  },
+  {
+    "ChallengeNumber": 96,
+    "Title": "CurrentlyInStock"
+  },
+  {
+    "ChallengeNumber": 97,
+    "Title": "Giveaway"
+  },
+  {
+    "ChallengeNumber": 98,
+    "Title": "Advertisement"
+  },
+  {
+    "ChallengeNumber": 99,
+    "Title": "Categories"
+  },
+  {
+    "ChallengeNumber": 100,
+    "Title": "RedesignDailyUILandingPage"
+  },
+]
